b6aa9a59beef64d4838f18b2852fd2bcb7b9841e
JakePhillip <panchee2548@gmail.com>
2026-09-01 11:53:49 +0700

    Adding essential web componets (pages but not yet programmed... testing the web output only)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+import './static/css_styles/index.css'
+import DmeExplorer from "./App.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* The DmeExplorer is a render component from the function */}
+    <DmeExplorer/>
   </StrictMode>,
 )
---
+++ src/static/pages/homePage.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "../css_styles/css_pages/homePage_style.css";
+
+function Home(){
+    return (
+        <div>
+            <h1>Welcome to DME Explorer</h1>
+            <p>These are some information</p>
+        </div>
+    )
+}
+
+export default Home
