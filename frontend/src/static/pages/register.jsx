5a3d2267fe9a129c70022baa8e7cc53c1119f795
JakePhillip <panchee2548@gmail.com>
2026-09-02 20:21:41 +0700

    style the homepage in CSS + make it reponsive for mobile devices

--- frontend/src/static/pages/register.jsx
+++ frontend/src/static/pages/register.jsx
@@ -14,8 +14,8 @@ export function Register(){
             
             <div className="register-information-content">
                 <p className="register-welcome">Welcome to Faculty of</p>
-                <h1 classname="register-digitalmedia-engineering">Digital Media Engineering</h1>
-                <div classname="register-images">
+                <h1 className="register-digitalmedia-engineering">Digital Media Engineering</h1>
+                <div className="register-images">
                     <img src= {GraduationCoEImage} alt = "CoE graduation students image"/>
                     <img src={DMEStudio} alt="DME student at studio" />
                 </div>
@@ -25,8 +25,52 @@ export function Register(){
                     Creating interesting digital experience by design, develop, interactive digital media 
                     <br className="desktop-break"/>with optimizing technology systems
                 </p>
+
+                <p className="register-footer-text">
+                    Come to join us and start the successful future career
+                </p>
             </div>
         </section>
+
+        <section className="register-form-section">
+            <div className="register-form-container">
+                <h2 className="account-create-title">Create your account</h2>
+
+                {/*The form for register*/}
+                <form>
+                    <div className="register-input-group">
+                        <label htmlFor="username">Username: </label>
+                        <input id="username" name="username" type="text" placeholder="Enter your username:" autoComplete="on" />
+                    </div>
+
+                    <div className="register-input-group">
+                        <label htmlFor="email">Email Address: </label>
+                        <input id="email" name="email" type="email" placeholder="Enter your email address: " autoComplete="yes" />
+                    </div>
+
+                    <div className="register-input-group">
+                        <label htmlFor="password">Password: </label>
+                        <input id="password" name="password" type="password" placeholder="Enter your password: " autoComplete="no" />
+                    </div>
+
+                    <div className="register-input-group">
+                        <label htmlFor = "password-confirmation">Password Confirmation</label>
+                        <input id="password-confirmation" name="passwordConfirmation" type="password" placeholder="Enter your confirmation password: " autoComplete="off" />
+                    </div>
+
+                    <div className="register-button-container">
+                        <button type="submit" className="register-button">Sign up</button>
+                    </div>
+                </form>
+                
+                <div className="register-login">
+                    <span className="have-acc-already">Already have an account? </span>
+                    <Link to="/login">Login</Link>
+                </div>
+            </div>
+        </section>
+
     </main>
     )
 }
+
